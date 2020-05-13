a355273e71641ac0cfdfcc50d03d8e11a201f20c
terrenceoconnor57 <60070754+terrenceoconnor57@users.noreply.github.com>
2020-05-12 20:54:07 -0400

    Port change for server

--- src/Welcome.js
+++ src/Welcome.js
@@ -10,10 +10,24 @@ class Welcome extends Component {
 
 
   return(
-<div>
-    <h1>Welcome</h1>
-    <h3>There has been a boom in virtual education
-    bla bla bla</h3>
+<div className = 'welcomediv'>
+    <h1>Welcome to UroEdCentral!</h1>
+    <h3>Due to the recent COVID-19 outbreak,
+    there has been a boom in virtual education
+    resources for urology. We designed this website
+    with one goal in mind: create an organized central
+    hub for all of these amazing resources. The page is
+     organized by subspeciality, making it easier for you
+     to browse the specific presentations that interest you.</h3>
+     <h3>In addition, we encourage you to submit educational material
+     directly to UroEdCentral as it becomes available! Just input the
+     necessary information at the top of each page with the corresponding
+     link to the video/presentation.</h3>
+     <h3>This page is very much a work in progress, so we are interested
+     in hearing your feedback! Please submit any questions, comments,
+     concerns, ect. to the email address listed below. We greatly
+     appreciate your interest in our website and hope that you find
+     this helpful during this difficult time.</h3>
 
     </div>
 
